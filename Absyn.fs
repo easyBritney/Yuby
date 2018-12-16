64032d79aea4d78015fa33d274dd6add2b669eea
easyBritney <18072712667@163.com>
2018-12-16 23:24:39 +0800

    粗写Lex部分

--- Absyn.fs
+++ Absyn.fs
@@ -1,22 +1,12 @@
-(* File MicroC/Absyn.fs
-   Abstract syntax of micro-C, an imperative language.
-   sestoft@itu.dk 2009-09-25
-
-   Must precede Interp.fs, Comp.fs and Contcomp.fs in Solution Explorer
- *)
 
 module Absyn
 
 type typ =
-  | TypI                             (* Type int                    *)
-  | TypC                             (* Type char                   *)
+  | TypI                             
+  | TypC                             
   | TypA of typ * int option         (* Array type                  *)
-  | TypP of typ                      (* Pointer type                *)
                                                                    
 and expr =                                                         
-  | Access of access                 (* x    or  *p    or  a[e]     *)
-  | Assign of access * expr          (* x=e  or  *p=e  or  a[e]=e   *)
-  | Addr of access                   (* &x   or  &*p   or  &a[e]    *)
   | CstI of int                      (* Constant                    *)
   | Prim1 of string * expr           (* Unary primitive operator    *)
   | Prim2 of string * expr * expr    (* Binary primitive operator   *)
@@ -24,14 +14,10 @@ and expr =
   | Orelse of expr * expr            (* Sequential or               *)
   | Call of string * expr list       (* Function call f(...)        *)
                                                                    
-and access =                                                       
-  | AccVar of string                 (* Variable access        x    *) 
-  | AccDeref of expr                 (* Pointer dereferencing  *p   *)
-  | AccIndex of access * expr        (* Array indexing         a[e] *)
-                                                                   
 and stmt =                                                         
   | If of expr * stmt * stmt         (* Conditional                 *)
   | While of expr * stmt             (* While loop                  *)
+  | Unless of expr * stmt *stmt   
   | Expr of expr                     (* Expression statement   e;   *)
   | Return of expr option            (* Return from method          *)
   | Block of stmtordec list          (* Block: grouping and scope   *)
@@ -41,7 +27,7 @@ and stmtordec =
   | Stmt of stmt                     (* A statement                 *)
 
 and topdec = 
-  | Fundec of typ option * string * (typ * string) list * stmt
+  | Fundec of typ option * string * (typ * string) list option * stmt  (* 可选参数 函数*)
   | Vardec of typ * string
 
 and program = 
