02e28ba16708be483c19f925eec2dceae489832d
Hhhuyu <charstal_h@outlook.com>
2019-01-12 12:41:28 +0800

    merge

---
+++ Cuby/testing/ex(if).c
@@ -0,0 +1,13 @@
+int main(){
+    int a = 2;
+    if (a == 1){
+        print 1;
+    }else if(a == 3){
+        print 3;
+    }else if(a == 4){
+        print 4;
+    }else if(a == 2){
+        print 2;
+    }
+
+}
